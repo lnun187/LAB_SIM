cb6f883f6651e29321525b8da5c7f953c0b6e51c
lnun187 <167327421+lnun187@users.noreply.github.com>
2024-09-12 12:34:51 +0700

    Ex6 Lab1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -86,14 +86,14 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  init_exercise_5();
+  init_exercise_6();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    exercise_5_run();
+    exercise_6_run();
     HAL_Delay(1000);
     /* USER CODE END WHILE */
 
